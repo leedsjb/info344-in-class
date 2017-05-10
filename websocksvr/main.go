2362d0675dbb2c0c9fe78f091b935978ef541c4b
J. Benjamin Leeds <leeds@uw.edu>
2017-05-10 15:23:38 -0700

    after class Wednesday May 10, websockets working

--- websocksvr/main.go
+++ websocksvr/main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 //HandlerContext provides the HTTP handlers with
@@ -25,6 +27,9 @@ type MessageEvent struct {
 //would create and broacast events in response to various handler
 //actions, e.g., new user sign-up, post of a new message, etc.
 func (ctx *HandlerContext) TriggerEvent(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("TriggerEventhandler Called")
+
 	//CORS headers to allow cross-origin requests
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Access-Control-Request-Method", "POST")
@@ -34,6 +39,21 @@ func (ctx *HandlerContext) TriggerEvent(w http.ResponseWriter, r *http.Request) 
 	//and the current time for CreatedAt
 	//Then pass the MessageEvent to the `.Notify()` method of your notifier
 	//so that the event gets broadcasted to all web socket clients
+
+	mE := &MessageEvent{
+		Message:   "web sockets are cool",
+		CreatedAt: time.Now(),
+	}
+
+	// trigger broadcast of new message event to all web socket clients
+	ctx.Notifier.Notify(mE)
+
+}
+
+var ugprader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true }, // override Gorilla cross origin check by always returning true
 }
 
 //WebSocketUpgradeHandler handles websocket upgrade requests
@@ -46,8 +66,15 @@ func (ctx *HandlerContext) WebSocketUpgradeHandler(w http.ResponseWriter, r *htt
 	//any origin.
 	//See https://godoc.org/github.com/gorilla/websocket#hdr-Origin_Considerations
 
+	conn, err := ugprader.Upgrade(w, r, nil) // TODO *** repsonse header???
+	if err != nil {
+		http.Error(w, "error upgrading connection"+err.Error(), http.StatusInternalServerError)
+	}
+
 	//after upgrading, use the `.AddClient()` method on your notifier
 	//to add the new client to your notifier's map of clients
+
+	ctx.Notifier.AddClient(conn)
 
 }
 
@@ -60,6 +87,7 @@ func main() {
 
 	//TODO: start the notifier by calling
 	//its .Start() method on a new goroutine
+	go ctx.Notifier.Start()
 
 	http.HandleFunc("/v1/ws", ctx.WebSocketUpgradeHandler)
 	http.HandleFunc("/v1/trigger", ctx.TriggerEvent)
--- websocksvr/notifier.go
+++ websocksvr/notifier.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"sync"
+
 	"github.com/gorilla/websocket"
 )
 
 //Notifier represents a web sockets notifier
 type Notifier struct {
 	eventq  chan interface{}
-	clients map[*websocket.Conn]bool
+	clients map[*websocket.Conn]bool // boolean value is meaningless, we use a map for fast lookup access of clients
+	mu      sync.RWMutex
 	//TODO: add other fields you might need
 	//such as another channel or a mutex
 	//(either would work)
@@ -20,7 +24,16 @@ type Notifier struct {
 func NewNotifier() *Notifier {
 	//TODO: create, initialize and return
 	//a Notifier struct
-	return nil
+
+	bufferSize := 10
+
+	notifier := &Notifier{
+		eventq:  make(chan interface{}, bufferSize), // make channel buffered with second param
+		clients: make(map[*websocket.Conn]bool),     // make new map w/ websocket.Conn as keys and bools as values (bools are meaningless)
+		mu:      sync.RWMutex{},
+	}
+
+	return notifier
 }
 
 //Start begins a loop that checks for new events
@@ -28,10 +41,16 @@ func NewNotifier() *Notifier {
 //This function should be called on a new goroutine
 //e.g., `go mynotifer.Start()`
 func (n *Notifier) Start() {
-	//TODO: implement this function
+	//TODO: implement this function w/ never ending for loop
 	//this should check for new events written
 	//to the `eventq` channel, and broadcast
 	//them to all of the web socket clients
+
+	for {
+		fmt.Println("broadcast loop running")
+		event := <-n.eventq
+		n.broadcast(event)
+	}
 }
 
 //AddClient adds a new web socket client to the Notifer
@@ -42,15 +61,24 @@ func (n *Notifier) AddClient(client *websocket.Conn) {
 	//processed on its own goroutine, so your
 	//implementation here MUST be safe for concurrent use
 
+	n.mu.Lock() // use mutex write lock to ensure map is safe for concurrent use
+	defer n.mu.Unlock()
+
+	clientEntry := n.clients[client]
+	if clientEntry == false { // go maps return the "0" value of values that don't exist at a given key
+		n.clients[client] = true
+	}
+
 }
 
 //Notify will add a new event to the event queue
 func (n *Notifier) Notify(event interface{}) {
 	//TODO: add the `event` to the `eventq`
+	n.eventq <- event // write event to eventq channel
 }
 
 //readPump will read all messages (including control messages)
-//send by the client and ignore them. This is necessary in order
+//sent by the client and ignore them. This is necessary in order to
 //process the control messages. If you don't do this, the
 //websocket will get stuck and start producing errors.
 //see https://godoc.org/github.com/gorilla/websocket#hdr-Control_Messages
@@ -59,9 +87,16 @@ func (n *Notifier) readPump(client *websocket.Conn) {
 	//Control Message section of the Gorilla Web Socket docs:
 	//https://godoc.org/github.com/gorilla/websocket#hdr-Control_Messages
 
+	for {
+		if _, _, err := client.NextReader(); err != nil { // if err is not nil, client has "wandered off" -> close socket
+			client.Close()
+			fmt.Printf("error reading w/ client.NextReader(): %v\n", err.Error())
+			break
+		}
+	}
 }
 
-//broadcast sends the event to all client as a JSON-encoded object
+//broadcast sends the event to all clients as a JSON-encoded object
 func (n *Notifier) broadcast(event interface{}) {
 	//TODO: Loop over all of the web socket clients in
 	//n.clients and write the `event` parameter to the client
@@ -70,6 +105,16 @@ func (n *Notifier) broadcast(event interface{}) {
 	//and for even better performance, try using a PreparedMessage:
 	//https://godoc.org/github.com/gorilla/websocket#PreparedMessage
 	//https://godoc.org/github.com/gorilla/websocket#Conn.WritePreparedMessage
+
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	for client := range n.clients { // iterate over each client in map
+		if err := client.WriteJSON(event); err != nil { // write event to client
+			client.Close()            // close connection to client
+			delete(n.clients, client) // delete client from map TODO ** safe for concurrent access ?? **
+		}
+	}
 
 	//If you get an error while writing to a client,
 	//the client has wandered off, so you should call
